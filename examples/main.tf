5230457df7745fc36e176080a491443cf487f849
James Phillips <phillips.james@gmail.com>
2025-06-24 07:57:30 -0700

    adding more examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,8 @@
 module "ci_user" {
-  source = "github.com/fortunecookiezen/aws-tf-ci-user"
-  name   = "ci-user"
-  path   = "/ci/"
+  source                  = "github.com/fortunecookiezen/aws-tf-ci-user"
+  name                    = "ci-user"
+  path                    = "/ci/"
+  ci_user_policy_document = data.aws_iam_policy_document.demo.json
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser",
     "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess",
@@ -13,5 +14,15 @@ module "ci_user" {
   ]
   tags = {
     Environment = "nonprod"
+  }
+}
+
+data "aws_iam_policy_document" "demo" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateTags"
+    ]
+    resources = ["*"]
   }
 }
